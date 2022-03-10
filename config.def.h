d64ee48dc4012335fc7517d4bfcd09dc3c1ae482
png261 <nhphuong.code@gmail.com>
2022-03-10 15:38:56 +0700

    change basic thing

--- config.def.h
+++ config.def.h
@@ -26,7 +26,7 @@ static char *colors[][3] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "いち", "に", "さん", "よん"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -52,7 +52,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \

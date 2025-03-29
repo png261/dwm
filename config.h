8c95fbd412db64629abdaa21fe58359902e163f8
png261 <nhphuong.code@gmail.com>
2025-03-29 07:58:06 +0700

    update config

--- config.h
+++ config.h
@@ -42,11 +42,6 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "firefox", NULL,     NULL,           1 << 0,    0,          0,          -1,        -1 },
-	{ TERMINAL_CLASS,  NULL,     NULL,           1 << 1,    0,          1,           0,        -1 },
-	{ "Chromium",NULL,     NULL,		   1 << 2,    0,          0,          -1,        -1 },
-	{ "Zathura", NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
-	{ "Anki", NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
